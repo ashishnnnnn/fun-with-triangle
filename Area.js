ff6a9c1b5cb16dd3c1695d05dc46029856b12939
ASHISH KUMAR <40332678+ashishnnnnn@users.noreply.github.com>
2021-08-28 14:26:37 +0530

    feat - coding

--- Area.js
+++ Area.js
@@ -1,18 +1,22 @@
 var side1 = document.querySelector(".first-side")
 var side2 = document.querySelector(".second-side");
+var side3 = document.querySelector(".third-side");
 var button = document.querySelector(".button")
 var message = document.querySelector(".message3");
 
-function get_area(side1,side2){
+function get_area(side1,side2,side3){
     var first = Number(side1);
     var second = Number(side2);
-    var area = (first*second)/2;
-    return area;
+    var third = Number(side3);
+    // console.log(first,second,third);
+    var semi_peri = (first+second+third)/2;
+    var temp = semi_peri*(semi_peri-first)*(semi_peri-second)*(semi_peri-third);
+    return Math.sqrt(temp);
 }
 
 function showmessage(){
-    var hypotenus = get_area(side1.value,side2.value);
-    message.innerText = "Area is "+hypotenus;
+    var area = get_area(side1.value,side2.value,side3.value);
+    message.innerText = "Area is "+area;
 }
 
 button.addEventListener("click",showmessage);
